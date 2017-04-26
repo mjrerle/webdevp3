56fb0888ef3c07ac515cba6686f05547dda96096
powhound4 <george.hatch@q.com>
2017-04-26 12:44:16 -0600

    Update ajax_ingredient.php

--- ajax_ingredient.php
+++ ajax_ingredient.php
@@ -1,21 +1,12 @@
 <?php
 include "templates/page_setup.php";
-include "templates/header.php";
-include "templates/jumbotron.php";
+header('Content-Type: text/json');
+header("Access-Control-Allow-Origin: *");
+if (isset($_GET['ing'])){
+ $db = new Database();
+ $ing = trim($_GET['ing'],'"');
+ $ingredient = $db->getIngredientbyName($ing);
+ $ingarray = array("name"=>$ingredient['i_name'], "short"=>$ingredient['description'], "unit"=>$ingredient['unit'],"cost"=>$ingredient['price'],"time"=>$ingredient['time'], "desc"=>$ingredient['longdescription']); //redient['i_name']
+ echo json_encode($ingarray);
+ }
 ?>
-<script src = "display.js"></script>
-<div class = "row">
-  <div class = "col-md-3">
-  </div>
-  <div class = "col-md-4">
-    <span id = "masterstatus"></span><br>
-    <span id = "debug"></span><br>
-    <span id = "debug2"></span><br>
-    <div class = "col-md-4">
-      <table id = "ingredients">
-      </table>
-    </div>
-  </div>
-</div>
-
-<?php include "templates/footer.php";?>
