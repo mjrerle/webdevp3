28ea743abbd3279696744d44ca4094fbf3ec95f4
mjrerle <matterle@live.com>
2017-04-27 17:50:54 -0600

    HEAD inclusion bug

--- ajax_ingrimage.php
+++ ajax_ingrimage.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 include "templates/page_setup.php";
 header('Content-Type: text/json');
@@ -15,18 +14,4 @@ if(isset($_GET['ing'])){
 }
 else
   header('location: index.php');
-
-=======
-include "templates/page_setup.php";
-header('Content-Type: text/json');
-header("Access-Control-Allow-Origin: *");
-if (isset($_GET['ing'])){
- $db = new Database();
- $ing = trim($_GET['ing'],'"');
- $ingredient = $db->getIngredientbyName($ing);//get initial ingredient
- $img = $ingredient['imgURL'];//get the img url ex sugar.png
- $imgArray = $db->getImageByName($img);//get the image info from the images table via the imgURL
- echo json_encode($imgArray);
- }
->>>>>>> 4b906efe875bdf895d310e2d022995ff2a106a84
 ?>
