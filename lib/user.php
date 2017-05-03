39d348f9f6d77c2966d1a2f0c4234679a5e98a2b
mjrerle <matterle@live.com>
2017-05-03 16:53:20 -0600

    removed old partner from user list

--- lib/user.php
+++ lib/user.php
@@ -38,7 +38,6 @@ class User {
 	  $users [$i ++] = new User ( 'mjrerle', 'mjrerle', 'Admin', 'matterle@live.com' );
     $users [$i ++] = new User ( 'ct310', 'A835E0', 'Admin', 'ct310@cs.colostate.edu' );
     $users [$i ++] = new User ( 'fred', '3B23E6', 'Customer', 'ct310@cs.colostate.edu' );
-    $users[$i ++] = new User('tjnolan', 'lukifer', 'Admin', 'tjnolan@rams.colostate.edu');
     User::writeUsers ( $users );
   }
 
